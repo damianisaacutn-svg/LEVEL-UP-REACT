9fc38603bbcb57b79af8134df6a6f4e1af28fbfa
damianisaacutn <damianisaacutn@gmail.com>
2026-03-24 11:06:57 -0600

    Eliminar contenido anterior de main

--- src/admin/components/Toast.jsx
+++
@@ -1,7 +0,0 @@
-export default function Toast({ message }) {
-  return (
-    <div className="fixed bottom-5 right-5 bg-[#2ECC71] text-white px-4 py-2 rounded-lg shadow-lg">
-      {message}
-    </div>
-  )
-}
